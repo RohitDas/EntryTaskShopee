38a287a41c44903c2eafe5dea98e04c47456be92
rohitangsu.das <rohitangsu.das@shopee.com>
2019-01-23 17:36:16 +0800

    adding modified run.sh

--- MapReducerComputation/hadoop/run.sh
+++ MapReducerComputation/hadoop/run.sh
@@ -1,41 +1,28 @@
-HERE=$(pwd)
-MINDEGREE=3
+#!/usr/bin/env bash
+HERE=`pwd`
 
-echo -e "\n  CHECKING INSTALLED SOFTWARE \n"
+echo $HERE
 
-GO=${GO:-$(which go)}
-
-if [ -z "$GO" ]; then
-    echo "    Please install Go (golang)"
-    exit 1
-else
-  echo "    GO=" $GO
+if [ ! -d "$HERE/bin/" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir $HERE/bin
 fi
-
 
 echo -e "\n  BUILD MAPPER AND REDUCER \n"
 
 cd $HERE/cmd/mapper && go build
+cp $HERE/cmd/mapper/mapper $HERE/bin/
 cd $HERE/cmd/reducer && go build
+cp $HERE/cmd/reducer/reducer $HERE/bin/
 cd $HERE/cmd/mapper2 && go build
+cp $HERE/cmd/mapper2/mapper2 $HERE/bin/
 cd $HERE/cmd/reducer2 && go build
-cd $HERE
+cp $HERE/cmd/reducer2/reducer2 $HERE/bin/
+cd $HERE/bin
+
 echo -e "\n  RUN HADOOP JOB \n"
 
-OUTPUT="./output"
-
-if [ -d "$OUTPUT" ]; then
-  echo "    Please delete $OUTPUT directory"
-  exit 1
-else
-  echo "    hadoop  jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-$HADOOP_VERSION.jar -input ./input -output ./output -mapper ./mapper/mapper -reducer ./reducer/reducer"
-
-  hadoop  jar /usr/local/Cellar/hadoop/3.1.1/libexec/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -input /rohit/test.data -output /rohit/out1 -mapper ./cmd/mapper/mapper -file ./cmd/mapper/mapper -reducer "./cmd/reducer/reducer" -file ./cmd/reducer/reducer
-
-  echo "    Check output directory to see the results"
-fi
-
-hadoop  jar /usr/local/Cellar/hadoop/3.1.1/libexec/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -input /rohit/out1/ -output /rohit/out2 -mapper ./cmd/mapper2/mapper2 -file ./cmd/mapper2/mapper2 -reducer ./cmd/reducer2/reducer2 -file ./cmd/reducer2/reducer2
-
+hadoop  jar /opt/hadoop-streaming/hadoop-streaming.jar -input /ads_tracking/2019-01-01/ -output /user/ld-rohitangsu_das/map1 -mapper "mapper" -file "mapper" -reducer "reducer" -file "reducer"
+hadoop  jar /opt/hadoop-streaming/hadoop-streaming.jar -input /user/ld-rohitangsu_das/map1/ -output /user/ld-rohitangsu_das/reduce1/ -mapper "mapper2" -file "mapper2" -reducer "reducer2" -file "reducer2"
 
 
